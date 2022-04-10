896718fa485541568e42640c5266db7749e8b9a9
willi <willi@192.168.1.135>
2022-04-10 14:39:36 +0200

    10/4/2022

--- src/fp/laboratorio/test/TestIgualdad.java
+++ src/fp/laboratorio/test/TestIgualdad.java
@@ -5,13 +5,13 @@ public class TestIgualdad {
 
 	public static void main(String[] args) {
 		Integer n1 = 205;
-		Integer n2 =Integer.valueOf("205");
+		Integer n2 =Integer.valueOf("206");
 		Integer n3 = 15;
 		Integer n4 = Integer.valueOf("15");
 		Integer n5 = 205;
 		Integer n6 = 15;
 		Integer n7 = n1;
-		System.out.println(n1.equals(n2));
+		System.out.println(n2.compareTo(n1));
 		System.out.println(n3.equals(n4));
 		System.out.println(n1.equals(n5));
 		System.out.println(n3.equals(n6));
